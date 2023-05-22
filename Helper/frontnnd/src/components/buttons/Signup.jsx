ae8264b7dfcb8dea70851c49f36aef556071861d
AbhayThagle01 <97532074+AbhayThagle01@users.noreply.github.com>
2023-05-22 15:35:24 +0530

    Update Signup.jsx

--- Helper/frontnnd/src/components/buttons/Signup.jsx
+++ Helper/frontnnd/src/components/buttons/Signup.jsx
@@ -1,74 +1,41 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 const Signup = () => {
-    const [user_name, setUsername] = useState("")
-    const [phone_no, setPhoneNo] = useState("")
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-
-    async function signupUser(event) {
-        event.preventDefault();
-        console.log("signup  function working");
-        const response = await fetch("http://localhost:5000/signup", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                user_name,
-                phone_no,
-                email,
-                password,
-            }),
-        });
-        const data = await response.json();
-        console.log(data);
-    }
-
     return (
         <div>
             {/* <!-- Button trigger modal --> */}
-            <button type="button" className="btn btn-outline-primary ms-2" data-bs-toggle="modal" data-bs-target="#signupModal">
+            {/* <button type="button" className="btn btn-outline-primary ms-2" data-bs-toggle="modal" data-bs-target="#signupModal">
                 <span className="fa fa-user-plus me-1"></span> Register
-            </button>
+            </button> */}
 
             {/* <!-- Modal --> */}
-            <div className="modal fade" id="signupModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div style={{height:"100vh",background:"linear-gradient(to bottom, white,#5fc3ef, white)"}} >
                 <div className="modal-dialog">
-                    <div className="modal-content">
+                    <div className="modal-content border-2 border-primary">
                         <div className="modal-header">
-                            <h5 className="modal-title" id="exampleModalLabel">Register</h5>
-                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            <h5 className="modal-title" id="exampleModalLabel">Signup</h5>
+                            {/* <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> */}
                         </div>
                         <div className="modal-body">
-                            <form onClick={signupUser}>
+                            <button className="btn btn-primary w-100 mb-4">
+                                <span className="fa fa-google me-2"></span> Sign up With Google
+                            </button>
+                            <button className="btn btn-primary w-100 mb-4">
+                                <span className="fa fa-facebook me-2"></span> Sign up With Facebook
+                            </button>
+                            <form>
                                 <div className="mb-3">
                                     <label htmlFor="exampleInput" className="form-label">Username</label>
-                                    <input type="text" className="form-control" id="exampleInput"
-                                        onChange={(e) => {
-                                            setUsername(e.target.value)
-                                        }} />
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="exampleInput" className="form-label">Phone no</label>
-                                    <input type="text" className="form-control" id="exampleInput"
-                                        onChange={(e) => {
-                                            setPhoneNo(e.target.value)
-                                        }} />
+                                    <input type="text" className="form-control" id="exampleInput" />
                                 </div>
                                 <div className="mb-3">
                                     <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-                                        onChange={(e) => {
-                                            setEmail(e.target.value)
-                                        }} />
+                                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
                                     <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                                 </div>
                                 <div className="mb-3">
                                     <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                                    <input type="password" className="form-control" id="exampleInputPassword1" onChange={(e) => {
-                                        setPassword(e.target.value)
-                                    }} />
+                                    <input type="password" className="form-control" id="exampleInputPassword1" />
                                 </div>
                                 <div className="mb-3 form-check">
                                     <input type="checkbox" className="form-check-input" id="exampleCheck1" />
