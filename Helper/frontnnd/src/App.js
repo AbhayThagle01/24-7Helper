fb346d53c24334e736f5d4e1c01f9ba4085f33c9
AbhayThagle01 <97532074+AbhayThagle01@users.noreply.github.com>
2023-05-22 15:36:13 +0530

    Update App.js

--- Helper/frontnnd/src/App.js
+++ Helper/frontnnd/src/App.js
@@ -7,7 +7,10 @@ import Product from './components/Product'
 import Contact from './components/Contact'
 import { Redirect, Route, Switch } from 'react-router-dom';
 import ProductDetail from './components/ProductDetail';
+import Cart from './components/Cart';
+import Checkout from './components/Checkout'
 import Login from './components/buttons/Login';
+import Signup from './components/buttons/Signup';
 
 function App() {
   return (
@@ -17,9 +20,12 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route exact path="/products" component={Product} />
         <Route exact path="/products/:id" component={ProductDetail}/>
+        <Route exact path="/cart" component={Cart} />
+        <Route exact path="/checkout" component={Checkout} />
         <Route exact path="/about" component={About} />
         <Route exact path="/contact" component={Contact} />
         <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={Signup} />
         <Redirect to="/" />
       </Switch>
       <Footer/>
